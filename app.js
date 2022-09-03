870b3f51597b723d4e0a956f92b121fbffc4aecc
MohdUmar <Mohdumar8300@gmail.com>
2022-09-03 20:34:42 +0530

    connected email to contact form

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("js file");
+const form = document.getElementById("contact-form");
+
+function sendEmail(){
+    var params = {
+        from_name : document.getElementById('fullName').value,
+        email_id : document.getElementById('email-id').value,
+        message : document.getElementById("message").value
+        }
+        emailjs.send("service_n6k1t4l", "template_vx55t8l", params).then(function(res){alert("Messages Sended " + res.status )})
+        form.reset()
+
+}
